ffa958fd1e7275a3068d64aa845b38f0f6b36cbd
Romain Haye <hayromain.rh@gmail.com>
2018-08-25 22:25:10 +0200

    Add error handling, update dashboard component to prefetch data

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,6 +6,7 @@ import { DataService } from './data.service';
 import { PlainLoggerService } from './plain-logger.service';
 import { throwIfAlreadyLoaded } from 'app/core/module-import-guard';
 import { BookTrackerErrorHandlerService } from './book-tracker-error-handler.service';
+import {BooksResolverService} from './books-resolver.service';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { BookTrackerErrorHandlerService } from './book-tracker-error-handler.ser
   ],
   declarations: [],
   providers: [
+    BooksResolverService,
     LoggerService,
     DataService,
     { provide: ErrorHandler, useClass: BookTrackerErrorHandlerService }
